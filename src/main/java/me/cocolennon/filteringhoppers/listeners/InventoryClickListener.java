01bcb993e113a26e18e2c12b875a41ef432994bf
Cocolennon <cocolennong@gmail.com>
2024-03-29 22:51:18 +0100

    Made it so you can't put two of the same item in the filter

--- src/main/java/me/cocolennon/filteringhoppers/listeners/InventoryClickListener.java
+++ src/main/java/me/cocolennon/filteringhoppers/listeners/InventoryClickListener.java
@@ -10,11 +10,13 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.Arrays;
+
 public class InventoryClickListener implements Listener {
     @EventHandler
     public void inventoryClick(InventoryClickEvent event) {
         if(!event.getView().getTitle().equals("§5Filtering Hoppers§f: §dFilter Menu")) return;
-        Player player = event.getWhoClicked().getKiller();
+        Player player = (Player) event.getWhoClicked();
         ItemStack current = event.getCurrentItem();
         Inventory inv = event.getInventory();
         Inventory getClickedInv = event.getClickedInventory();
@@ -25,11 +27,15 @@ public class InventoryClickListener implements Listener {
             event.setCancelled(true);
             if(click.isLeftClick()) {
                 int slot = MenuCreator.getInstance().getFirstFreeSlot(inv);
+                if(getClickedInv.equals(inv)) return;
                 if(slot == 2001) {
                     player.sendMessage("§d[§5Filtering Hoppers§d] §cThe filter is full!");
                     return;
                 }
-                if(getClickedInv.equals(inv)) return;
+                if(Arrays.stream(inv.getContents()).toList().contains(current)) {
+                    player.sendMessage("§d[§5Filtering Hoppers§d] §cThis item is already in the filter!");
+                    return;
+                }
                 inv.setItem(slot, current);
             }else if(click.isRightClick()) {
                 if(!getClickedInv.equals(inv)) return;
