6aac1198815f8ca3007224500ccfb1fd19ad1486
Cocolennon <cocolennong@gmail.com>
2024-03-29 22:56:22 +0100

    Removed unused import

--- src/main/java/me/cocolennon/filteringhoppers/listeners/InventoryClickListener.java
+++ src/main/java/me/cocolennon/filteringhoppers/listeners/InventoryClickListener.java
@@ -5,7 +5,6 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
