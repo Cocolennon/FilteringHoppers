669af4ceefb0609742564e7b59c8d7c0f86b1774
Cocolennon <corentin71.lopez@gmail.com>
2026-05-30 15:43:39 +0200

    Renamed class MenuCreator to MenuUtil and added item destruction toggle

--- src/main/java/me/cocolennon/filteringhoppers/utils/MenuUtil.java
+++ src/main/java/me/cocolennon/filteringhoppers/utils/MenuUtil.java
@@ -13,17 +13,29 @@ import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.List;
 
-public class MenuCreator {
-    public static void createFilterMenu(List<ItemStack> filter, Player player, Block block, boolean whitelist) {
+public class MenuUtil {
+    public static void createFilterMenu(List<ItemStack> filter, Player player, Block block, boolean whitelist, boolean shouldDestroy) {
         Hopper hopper = (Hopper) block.getState();
         FilterInventoryHolder invHolder = new FilterInventoryHolder(Main.getInstance(), 27, Localization.get(player, "menu-title", false), block);
         Inventory inv = invHolder.getInventory();
         if(filter != null) for(ItemStack itemStack : filter) invHolder.addItem(itemStack);
+        invHolder.setItem(24, getDestroyItem(player, shouldDestroy));
         invHolder.setItem(25, getModeItem(player, whitelist));
         invHolder.fillEmpty(18, getFillerItem());
         player.playSound(player.getLocation(), Sound.BLOCK_BARREL_OPEN, 1.0f, 1.0f);
         player.openInventory(inv);
         player.sendMessage(Localization.get(player, "filter.open", true));
+    }
+
+    public static ItemStack getDestroyItem(Player player, boolean shouldDestroy) {
+        ItemStack item = new ItemStack(shouldDestroy ? Material.RED_STAINED_GLASS_PANE : Material.GREEN_STAINED_GLASS, 1);
+        ItemMeta meta = item.getItemMeta();
+        assert meta != null;
+        meta.displayName(Localization.get(player, "filter.mode-item." + (shouldDestroy ? "destroy" : "keep") + ".name", false));
+        meta.lore(List.of(Localization.get(player, "filter.mode-item." + (shouldDestroy ? "destroy" : "keep") + ".hint", false)));
+        Helper.setButtonAction(meta, "toggleDestroy");
+        item.setItemMeta(meta);
+        return item;
     }
 
     public static ItemStack getModeItem(Player player, boolean isWhitelist) {
