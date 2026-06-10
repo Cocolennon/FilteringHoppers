999e9b8637da3ec4a18e7405abd8e5e552daa295
Cocolennon <corentin71.lopez@gmail.com>
2026-06-10 15:37:06 +0200

    Added new filter modes toggles to menu

--- src/main/java/me/cocolennon/filteringhoppers/utils/MenuUtil.java
+++ src/main/java/me/cocolennon/filteringhoppers/utils/MenuUtil.java
@@ -7,7 +7,7 @@ import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.Sound;
 import org.bukkit.block.Block;
-import org.bukkit.block.Hopper;
+import org.bukkit.block.TileState;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
@@ -20,13 +20,20 @@ import java.util.List;
 public class MenuUtil {
     public static NamespacedKey buttonAction = new NamespacedKey(Main.getInstance(), "buttonAction");
 
-    public static void createFilterMenu(List<ItemStack> filter, Player player, Block block, boolean whitelist, boolean shouldDestroy) {
-        Hopper hopper = (Hopper) block.getState();
+    public static void createFilterMenu(TileState hopperState, List<ItemStack> filter, Player player, Block block) {
         FilterInventoryHolder invHolder = new FilterInventoryHolder(Main.getInstance(), 27, Localization.get(player, "menu-title", false), block);
         Inventory inv = invHolder.getInventory();
         if(filter != null) for(ItemStack itemStack : filter) invHolder.addItem(itemStack);
+        boolean shouldDestroy = Helper.shouldDestroy(hopperState);
+        boolean isWhitelist = Helper.isWhitelist(hopperState);
+        boolean isFilterType = Helper.isFilterType(hopperState);
+        boolean isFilterMeta = Helper.isFilterMeta(hopperState);
+        boolean isFilterEnchanted = Helper.isFilterEnchanted(hopperState);
+        invHolder.setItem(19, getItem(player, isFilterType ? Material.GREEN_STAINED_GLASS_PANE : Material.RED_STAINED_GLASS_PANE, isFilterType ? "filter-type" : "ignore-type", "toggleType"));
+        invHolder.setItem(20, getItem(player, isFilterMeta ? Material.GREEN_STAINED_GLASS_PANE : Material.RED_STAINED_GLASS_PANE, isFilterType ? "filter-meta" : "ignore-meta", "toggleMeta"));
+        invHolder.setItem(21, getItem(player, isFilterEnchanted ? Material.GREEN_STAINED_GLASS_PANE : Material.RED_STAINED_GLASS_PANE, isFilterEnchanted ? "filter-enchanted" : "ignore-enchanted", "toggleEnchanted"));
         invHolder.setItem(24, getItem(player, shouldDestroy ? Material.RED_STAINED_GLASS_PANE : Material.GREEN_STAINED_GLASS_PANE, shouldDestroy ? "destroy" : "keep", "toggleDestroy"));
-        invHolder.setItem(25, getItem(player, whitelist ? Material.WHITE_STAINED_GLASS_PANE : Material.BLACK_STAINED_GLASS_PANE, whitelist ? "whitelist" : "blacklist", "toggleMode"));
+        invHolder.setItem(25, getItem(player, isWhitelist ? Material.WHITE_STAINED_GLASS_PANE : Material.BLACK_STAINED_GLASS_PANE, isWhitelist ? "whitelist" : "blacklist", "toggleMode"));
         invHolder.fillEmpty(18, getFillerItem());
         player.playSound(player.getLocation(), Sound.BLOCK_BARREL_OPEN, 1.0f, 1.0f);
         player.openInventory(inv);
